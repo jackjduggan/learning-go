e0f0361b709b6c217978a05ec49945df05a9afa2
jackjduggan <20094010@mail.wit.ie>
2024-08-07 15:59:35 +0100

    variables

--- tutorial.go
+++ tutorial.go
@@ -4,5 +4,12 @@ package main
 import "fmt" // fmt is a standard module, almost always imported. Allows for printing things out to the console.
 
 func main() {
-	fmt.Println("hello world") // single quotes doesn't work for strings in Go. You use '' for char instead.
+
+	// variables - var {name} {type} = {value}
+	var name string = "Hello Jack"
+	var number uint = 1000 // declaring a value and not using it produces an error in Go.
+
+	fmt.Println("Hello World") // single quotes doesn't work for strings in Go. You use '' for char instead.
+	fmt.Println(name)
+	fmt.Println(number)
 }
