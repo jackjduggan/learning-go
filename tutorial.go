9a0744f82184b9c3d5e4cb6d52afe474f8824688
jackjduggan <20094010@mail.wit.ie>
2024-08-08 10:20:59 +0100

    implicit/explicit

--- tutorial.go
+++ tutorial.go
@@ -7,9 +7,12 @@ func main() {
 
 	// variables - var {name} {type} = {value}
 	var name string = "Hello Jack"
-	var number uint = 1000 // declaring a value and not using it produces an error in Go.
+	//var number uint = 1000 // declaring a value and not using it produces an error in Go.
+	number := 6          // walrus operator, fasted way to assign a variable
+	boolean_var := false // booleans must be lowercase
+	// all types have a default value if you don't give them a value.
 
 	fmt.Println("Hello World") // single quotes doesn't work for strings in Go. You use '' for char instead.
-	fmt.Println(name)
-	fmt.Println(number)
+	fmt.Println(name, boolean_var)
+	fmt.Printf("%T", number) // %T displays the type
 }
